51263890bb3fd3c4568d7d14823f73ff6baa517d
Nikita <nik_mos2@mail.ru>
2023-05-13 18:08:07 +0300

    fisrt commit just to save progress
    added a bot core: "/stsrt" processing and logs

---
+++ src/main/java/com/uninvitedGuest/AnUninvitedGuestBot/AnUninvitedGuestBotApplication.java
@@ -0,0 +1,13 @@
+package com.uninvitedGuest.AnUninvitedGuestBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnUninvitedGuestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnUninvitedGuestBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uninvitedGuest/AnUninvitedGuestBot/AnUninvitedGuestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninvitedGuest.AnUninvitedGuestBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnUninvitedGuestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
